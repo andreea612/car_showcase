b94596ffd8a82f7e7ef53c6288ba368cea92ef3a
Andreia <andreeaaddisonb@gmail.com>
2025-05-22 23:11:02 +0300

    Initial commit

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomFilter from './Customfilter';
+import SearchBar from './Searchbar';
+import SearchManufacturer from './SearchManufacturer';
+
+export{
+   Hero,
+   CustomButton,
+   Navbar,
+   Footer,
+   SearchBar,
+   CustomFilter,
+   SearchManufacturer
+}
