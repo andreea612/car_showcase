4a2a791acbd55f6865a89dabe254344d62777de2
Andreia <andreeaaddisonb@gmail.com>
2025-05-25 11:55:08 +0300

    Am implementat funcții de calcul, pentru a afla rata de arendă a mașinilor pe an.

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,18 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manifacturer: string) => void;
 }
+
+export interface CarProps {
+city_mpg:number
+class:"compact car"
+combination_mpg:number
+cylinders:4
+displacement:1.6
+drive:string
+fuel_type:string
+highway_mpg:26
+make: string
+model:string
+transmission:string
+year:number
+}
